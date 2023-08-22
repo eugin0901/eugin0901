8674146cf1ec94704fddaf9941c98a403e54202e
eugin0901 <140703728+eugin0901@users.noreply.github.com>
2023-08-22 23:46:59 +0900

    확인2

--- studentclass1.py
+++ studentclass1.py
@@ -1,2 +1,9 @@
-from classstudent import Students
+from ClassStudent import Students
 Students
+
+
+# 학생을 한 명씩 반복합니다.
+print("이름", "총점", "평균", sep="\t")
+for student in students:
+    # 출력합니다.
+    print(student.to_string())
