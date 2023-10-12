db965382110bc8df0bf9c6da177c5f363922e74e
eugin0901 <140703728+eugin0901@users.noreply.github.com>
2023-10-12 22:10:29 +0900

    Delete flask_basic.py

--- flask_basic.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
-
-if __name__ == '__main__':
-    app.run(host = '127.0.0.1', debug=True, port=5000)
